302c124a44ad7de2161bcc8a9ea535b49cdd6745
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-01 18:11:22 +0530

    Add tags to AWS EC2 instance in main.tf

--- Day01/main.tf
+++ Day01/main.tf
@@ -2,5 +2,7 @@ resource "aws_instance" "my_ec2"{
     ami = "ami-0150f017fff1a46de"
     instance_type = "t3.micro"
     key_name = "mumbai"
-    #End
+    tags = {
+        Name = "terraform_ec2"
+    }
 }
