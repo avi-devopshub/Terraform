dfd3c72ceb4b4f12a4a25f5f06bcd6d965394b66
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-03 22:55:03 +0530

    Fix formatting in private route table resource

--- Day03/main.tf
+++ Day03/main.tf
@@ -55,7 +55,7 @@ resource "aws_route_table" "private_rt"{
     route{
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_nat_gateway.nat.id
-        }   
+        }    
     tags = {
         Name = "private_rt"
     }
