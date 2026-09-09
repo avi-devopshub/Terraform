0db817d2375f83360cad25018145744fe9aab6db
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-09 20:42:54 +0530

    Add backend configuration for S3 in Terraform provider

--- workspace/provider.tf
+++ workspace/provider.tf
@@ -11,3 +11,14 @@ provider "aws" {
   region = "ap-south-2"
   profile = "dev"
 }
+
+terraform {
+    backend "s3"{
+        bucket = "remote-backend-09092026"
+        region = "ap-south-2"
+        profile = "dev"
+        shared_credentials_files = ["/root/.aws/credentials"]
+        key = "dev/terraform.tfstate"
+        use_lockfile = true
+    }
+}
